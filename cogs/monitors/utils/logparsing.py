29341f6254e443e30a4b90a77fb43eb2a1274224
junepark678 <40409848+junepark678@users.noreply.github.com>
2023-01-29 15:29:03 +0000

    Add build string and product into logparsing.py (#6)
    * Update logparsing.py
    
    * Update logparsing.py
    
    * Update logparsing.py

--- cogs/monitors/utils/logparsing.py
+++ cogs/monitors/utils/logparsing.py
@@ -36,8 +36,15 @@ class LogParsing(commands.Cog):
         if json is not None:
             if "panicString" in json:
                 string = json['panicString'].split("\n")[0]
-                if not "```" in string or "@everyone" in string or "@here" in string:
-                    await msg.reply(f"Hey, it looks like this is a panic log!\n\nHere is the panic string:```{string}```")
+                
+            if "build" in json:
+                build = json['build'].split("\n")[0]
+                
+            if "product" in json:
+                product = json['product'].split("\n")[0]
+            
+            if (not "```" in string or "@everyone" in string or "@here" in string) and (not "`" in build or "@everyone" in build or "@here" in build) and (not "`" in product or "@everyone" in product or "@here" in product):
+                    await msg.reply(f"Hey, it looks like this is a panic log for build: `{build}` on a `{product}`!\n\nHere is the panic string:```{string}```")
 
     async def do_log_file(self, msg: discord.Message, att):
         text = await fetch_remote_file(att.url)
